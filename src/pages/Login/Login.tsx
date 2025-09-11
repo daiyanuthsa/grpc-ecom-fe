324285f9a562df7bbfa89ae042e2f31778e210cd
daiyanuthsa <95560026+daiyanuthsa@users.noreply.github.com>
2025-09-11 20:28:26 +0700

    feat: add protobuf submodule

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,34 +1,73 @@
-import { Link } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import FormInput from "../../components/FormInput/FormInput";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const loginschema = yup.object().shape({
+  email: yup.string().email("Email tidak valid").required("Email wajib diisi"),
+  password: yup.string().required("Kata sandi wajib diisi").min(6, "Kata sandi minimal 6 karakter"),
+});
+
+interface LoginFromValues {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
-    return (
-        <div className="login-section">
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-6 col-lg-5">
-                        <div className="login-wrap p-4">
-                            <h2 className="section-title text-center mb-5">Masuk</h2>
-                            <form action="#" className="login-form">
-                                <div className="form-group mb-4">
-                                    <input type="email" className="form-control" placeholder="Alamat Email" required />
-                                </div>
-                                <div className="form-group mb-4">
-                                    <input type="password" className="form-control" placeholder="Kata Sandi" required />
-                                </div>
-                                <div className="form-group">
-                                    <button type="submit" className="btn btn-primary btn-block">Masuk</button>
-                                </div>
-                                <div className="text-center mt-4">
-                                    <p>Belum punya akun? <Link to="/register" className="text-primary">Daftar di sini</Link>
-                                    </p>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+    const form = useForm<LoginFromValues>({
+        resolver: yupResolver(loginschema),
+    })
+    const submitHandler = (values: LoginFromValues) => {
+        console.log(values)
+    }
+  return (
+    <div className="login-section">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-6 col-lg-5">
+            <div className="login-wrap p-4">
+              <h2 className="section-title text-center mb-5">Masuk</h2>
+              <form
+                onSubmit={form.handleSubmit(submitHandler)}
+                className="login-form"
+              >
+                <FormInput<LoginFromValues>
+                  type="email"
+                  placeholder="Alamat Email"
+                  register={form.register}
+                  errors={form.formState.errors}
+                  name="email"
+                />
+                <FormInput<LoginFromValues>
+                  type="password"
+                  placeholder="Kata Sandi"
+                  register={form.register}
+                  errors={form.formState.errors}
+                  name="password"
+                />
+                <div className="form-group">
+                  <button type="submit" className="btn btn-primary btn-block">
+                    Masuk
+                  </button>
                 </div>
+                <div className="text-center mt-4">
+                  <p>
+                    Belum punya akun?{" "}
+                    <Link to="/register" className="text-primary">
+                      Daftar di sini
+                    </Link>
+                  </p>
+                </div>
+              </form>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
+
+
